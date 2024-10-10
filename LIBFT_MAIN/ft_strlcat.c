21e6c4f42ed5745a01ce4d233e324d1b37d26802
Yunus Emre Can Sümeralp <39301904+emresmrlp@users.noreply.github.com>
2024-10-10 09:34:00 +0300

    Add files via upload

--- LIBFT_MAIN/ft_strlcat.c
+++ LIBFT_MAIN/ft_strlcat.c
@@ -1,21 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysumeral <ysumeral@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 09:30:15 by ysumeral          #+#    #+#             */
+/*   Updated: 2024/10/10 09:32:54 by ysumeral         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-    size_t	i;
-    size_t	srcsize;
+	size_t	srclen;
+	size_t	dstlen;
+	size_t	i;
 
-    srcsize = 0;
-    while (src[srcsize] != '\0')
-      	srcsize++;
-    i = 0;
-    if (dstsize == 0)
-   		return (srcsize);
-    while (dstsize - 1 > i && src[i] != '\0')
-    {
-    	dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-    return (srcsize);
+	srclen = ft_strlen(src);
+	dstlen = ft_strlen(dst);
+	i = 0;
+	if (dstsize <= dstlen)
+		return (srclen + dstsize);
+	while (i < srclen && dstsize > (dstlen + i + 1))
+	{
+		dst[dstlen + i] = src[i];
+		i++;
+	}
+	if (dstlen + i < dstsize)
+		dst[dstlen + i] = '\0';
+	return (srclen + dstlen);
 }
---
+++ LIBFT_MAIN/libft.h
@@ -0,0 +1,8 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdlib.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlen(const char *s);
+
+#endif
